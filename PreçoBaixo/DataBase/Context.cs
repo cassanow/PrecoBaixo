7238051c9be9f2153226afa17c79c390b516ef6b
Arthur <nashcassano8@gmail.com>
2025-08-09 13:21:07 -0300

    Inicializando meu banco de dados com SQL Server

---
+++ PreçoBaixo/DataBase/Context.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PreçoBaixo.DataBase;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base (options) {}
+    
+}
