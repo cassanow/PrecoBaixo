c9711ae23d9622876603de35281a5cd6592d11fb
Arthur <nashcassano8@gmail.com>
2025-08-27 15:56:29 -0300

    Terminando de implementar minha Repository nos Testes Unitarios

---
+++ PreçoBaixoTest/Model/DbFixture.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using PreçoBaixo.DataBase;
+
+namespace PreçoBaixoTest.Model;
+
+public class DbFixture : IDisposable
+{
+    public Context Context {get; set;}
+    public SqliteConnection Connection {get; set;}
+
+    public DbFixture()
+    {
+        Connection  = new SqliteConnection("Datasource=:memory:");
+        Connection.Open();
+        
+        var options = new DbContextOptionsBuilder<Context>()
+            .UseSqlite(Connection)
+            .Options;
+        
+        Context = new Context(options);
+        Context.Database.EnsureCreated();
+    }
+
+    public void Dispose()
+    {
+        Context.Dispose();
+        Connection.Dispose();
+    }
+
+    public void Limpar()
+    {
+        Context.Usuarios.RemoveRange(Context.Usuarios); 
+        Context.SaveChanges();  
+    }
+}
--- PreçoBaixoTest/Repository/UsuarioRepositoryTests.cs
+++ PreçoBaixoTest/Repository/UsuarioRepositoryTests.cs
@@ -1,39 +1,93 @@
-﻿using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
-using PreçoBaixo.DataBase;
+﻿using Microsoft.EntityFrameworkCore;
 using PreçoBaixo.Models;
 using PreçoBaixo.Repository;
+using PreçoBaixo.Repository.Interface;
+using PreçoBaixoTest.Model;
 
 namespace PreçoBaixoTest.Repository;
 
-public class UsuarioRepositoryTests 
+public class UsuarioRepositoryTests : IClassFixture<DbFixture>
 {
-    private DbContextOptions<Context> CriarOptions(SqliteConnection connection)
+    
+    private readonly DbFixture _context;
+    private readonly IUsuarioRepository _usuarioRepository;
+
+    public UsuarioRepositoryTests(DbFixture context)
     {
-        return  new DbContextOptionsBuilder<Context>()
-            .UseSqlite(connection)
-            .Options;           
+        _context = context;
+        _context.Limpar();
+        _usuarioRepository = new UsuarioRepository(_context.Context);
+    }
+
+    private Usuarios CriarUsuario()
+    {
+        return new Usuarios
+        {
+            Id = 1,
+            Nome = "Arthur",
+            Email = "arthur@gmail.com",
+            Senha = "123456",
+            DataCadastro = DateTime.Now,
+        };
     }
     
     [Fact]
-    public void DeveRetornarUsuarios()
+    public async Task DeveRetornarUsuarios()
     {
-        var connection = new SqliteConnection("DataSource=:memory:");   
-        connection.Open();  
+        var usuario = CriarUsuario();
+        await _usuarioRepository.AddUsuario(usuario);
         
-        var options = CriarOptions(connection); 
-        var context = new Context(options);
-        context.Database.EnsureCreated();
-
-        var repository = new UsuarioRepository(context);
-        var usuario = new Usuarios
-            { Nome = "Arthur", Email = "arthur@gmail.com", Senha = "123456", DataCadastro = DateTime.Now };
-        repository.AddUsuario(usuario);
-        
-        var usuarios =  context.Usuarios.ToList();
+        var usuarios =  await _context.Context.Usuarios.ToListAsync();
         Assert.NotNull(usuarios);
         Assert.NotEmpty(usuarios);
-        Assert.Single(usuarios);
-        Assert.Equal("Arthur", usuarios[0].Nome);
+        Assert.Equal(usuario.Nome, usuarios[0].Nome);
+    }
+    
+    [Fact]
+    public async Task DeveAdicionarUsuario()
+    {
+        var usuario =  CriarUsuario();
+        
+        await _usuarioRepository.AddUsuario(usuario);
+        
+        Assert.NotNull(usuario);
+        Assert.Equal("Arthur", usuario.Nome);
+        Assert.Equal("arthur@gmail.com", usuario.Email); 
+        Assert.Equal("123456", usuario.Senha);
+    }
+
+    [Fact]
+    public async Task  DeveBuscarPorId()
+    {
+        var usuario = CriarUsuario();
+        await _usuarioRepository.AddUsuario(usuario);
+
+        var resultado = await _usuarioRepository.ObterPorId(usuario.Id);
+        Assert.NotNull(resultado);
+        Assert.Equal(usuario.Email, resultado.Email);
+    }
+    
+    [Fact]
+    public async Task DeveBuscarPorEmail()
+    {
+        var usuario = CriarUsuario();
+        await _usuarioRepository.AddUsuario(usuario);
+        
+        var resultado = await _usuarioRepository.ObterPorEmail(usuario.Email);
+        Assert.NotNull(resultado);
+        Assert.Equal(usuario.Email, resultado.Email);
+    }
+
+    [Fact]
+    public async Task DeveRemoverUsuario()
+    {
+        var usuario = CriarUsuario();
+        await _usuarioRepository.AddUsuario(usuario);
+        
+        var resultado = await _usuarioRepository.ObterPorId(usuario.Id);
+        await _usuarioRepository.DeleteUsuario(resultado);
+        
+        var usuarioRemovido = await  _usuarioRepository.ObterPorId(usuario.Id);
+        Assert.Null(usuarioRemovido);
     }
 }
